50a580876dfaf86e022137b96948a82e942f3d85
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2023-12-20 09:49:17 +0100

    made changes

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { useState } from "react";
 export default function App() {
+  const [loginData, setLoginData] = useState({});
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.logo}>
+        <FontAwesome5 name="piggy-bank" size={100} color="#1e56a0" />
+      </View>
+      <View style={{ marginBottom: 40 }}>
+        <Text style={styles.productTitle}>BANK VEST</Text>
+      </View>
+
+      <View style={styles.textView}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Username"
+          selectionColor={"#1e56a0"}
+          onChangeText={(e) => setLoginData({ ...loginData, username: e })}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          selectionColor={"#1e56a0"}
+          onChangeText={(e) => setLoginData({ ...loginData, password: e })}
+        />
+      </View>
+      <View style={styles.loginButtonView}>
+        <TouchableOpacity onPress={() => console.log(loginData)}>
+          <View style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>LOGIN</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  logo: {
+    marginBottom: 30,
+  },
+  productTitle: {
+    fontSize: 24,
+    color: "#1e56a0",
+  },
+  textInput: {
+    height: 50,
+
+    borderWidth: 1.5,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: "#163172",
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  textView: {
+    width: "90%",
+  },
+  loginButtonView: { width: "40%" },
+  loginButton: {
+    backgroundColor: "#f76b8a",
+    padding: 10,
+    borderRadius: 10,
+  },
+  loginButtonText: {
+    color: "#fffafa",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "600",
   },
 });
